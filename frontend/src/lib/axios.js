a57b858aecaf0e8b85aa3629f3239b49bb28f992
Ritika Sah <ritikasah731@gmail.com>
2025-05-08 21:53:33 +0530

    Deploying

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-application-backend-4me4.onrender.com" : "/api",
   withCredentials: true,
 });
