4ceaab96ec48467432df4498ac9ff721d30f2be2
Ritika Sah <ritikasah731@gmail.com>
2025-05-10 14:24:56 +0530

    Changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:  "https://chat-application-backend-4me4.onrender.com" ,
+  baseURL:  "https://chat-application-backend-4me4.onrender.com/api" ,
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL =  "https://chat-application-backend-4me4.onrender.com"
+const BASE_URL =  "https://chat-application-backend-4me4.onrender.com/api"
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
